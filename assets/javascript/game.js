948216cfcb5c4908e8624ab17a46573773604dda
tomcali <tmiller@toutbay.com>
2017-02-25 06:01:44 -0800

    initial game code structure set with bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,193 +8,62 @@ document.addEventListener("DOMContentLoaded", function() {
 // document.addEventListener("startButton", "click", function() {
 
 // declare variables up front
-    var numberGuessesAllowed = 3; // hangman-like rules set constant
+    var numberGames = 3; // ballgames to be played
     // regarding rules... set low value for testing
-    var indexOfTerm;
-    var selectedTerm;
-    var numberTargetLetters;
-    var target;
-    var termLetters;
-    var termLetterSet;
-    var alphabet;
-    var alphabetSet;
-    var availableLetterSet;
-    var usedLetterSet;
-    var numberBadGuesses; 
-    var continueGame; 
-    var responseValid;
-    var promptGuessLetter;
-    var thisGuessLetter;
-    var targetSet;
-    var availableLetterArray;
-    var usedLetterArray;
+    var indexOfGame;
+    var selectedAwayTeam;
+    var selectedHomeTeam;
+    var americanSet;
+    var nationalSet;
+    var americanUsedSet;
+    var nationalUsedSet; 
+    var numberAmericanChosen; 
+    var numberNationalChosen;
+    var leaguePreferenceScore;
 
 // identify DOM elements up front 
-    var oneLetterText = document.getElementById("oneLetterText");
-    var statusText = document.getElementById("statusText");
-    var definitionText = document.getElementById("definitionText");
-    var hintText = document.getElementById("hintText");
-    var correctTerm = document.getElementById("correctTerm");
-    var guessesRemaining = document.getElementById("guessesRemaining");
-    var lettersAvailable = document.getElementById("lettersAvailable");
-    var lettersUsed = document.getElementById("lettersUsed");
-    var targetLetters = document.getElementById("targetLetters");
-    var startButton = document.getElementById("startButton")
- 
+    var startButton = document.getElementById("startButton");
+    var awayTeamButton = document.getElementById("awayTeamButton");
+    var homeTeamButton = document.getElementById("homeTeamButton");
+    var gameResultText = document.getElementById("gameResultText");
+
+// set american and national icon sets as arrays and then sets of strings
+    var americanIconArray = ['logo_ana_79x76.jpg',  'logo_cws_79x76.jpg',  'logo_min_79x76.jpg',
+                        'logo_tb_79x76.jpg', 'logo_bal_79x76.jpg',  'logo_det_79x76.jpg',
+                        'logo_nyy_79x76.jpg',  'logo_tex_79x76.jpg', 'logo_bos_79x76.jpg',
+                        'logo_hou_79x76.jpg',  'logo_oak_79x76.jpg',  'logo_tor_79x76.jpg',
+                        'logo_cle_79x76.jpg',  'logo_kc_79x76.jpg',   'logo_sea_79x76.jpg'];
+    var americanIconSet = new Set(americanIconArray);
+
+    var nationalIconArray = ['logo_ari_79x76.jpg', 'logo_col_79x76.jpg',  'logo_nym_79x76.jpg',
+                        'logo_sf_79x76.jpg', 'logo_atl_79x76.jpg',  'logo_la_79x76.jpg',
+                        'logo_phi_79x76.jpg', 'logo_stl_79x76.jpg', 'logo_chc_79x76.jpg',
+                        'logo_mia_79x76.jpg', 'logo_pit_79x76.jpg',  'logo_was_79x76.jpg',
+                        'logo_cin_79x76.jpg',  'logo_mil_79x76.jpg',  'logo_sd_79x76.jpg'];
+    var nationalIconSet = new Set(nationalIconArray);
+
  startButton.onclick = function() {
     // readGameData.js contains an array of JSON objects for the game items
     console.log('-------------------------------');
     console.log('-------- START NEW GAME -------');
     console.log('-------------------------------');
-    console.log('the game data represent an object with statistical terms,');
-    console.log('short hints, and complete definitions of the terms.,');
-    console.log(gameData);
+    console.log('the game data consists of a set of ' +  numberGames + 
+        ' hypothetical baseball games');
 
-    // choose an term at random
-    indexOfTerm = getRandomInt(0, gameData.length - 1);
-    selectedTerm = gameData[indexOfTerm];
-
-    console.log('selected term:', selectedTerm['term']);
-    console.log('selected hint:', selectedTerm['hint']);
-    console.log('selected definition:', selectedTerm['definition']);
-
-    numberTargetLetters = selectedTerm['term'].length;
-    console.log('number of letters in target: ', numberTargetLetters);
-
-    target = []; // declare target array for display of solution
-    for (i = 0; i < numberTargetLetters; i++) {
-        target.push('_');
-    }
-    console.log('initial target has: ' + target.length + ' blank characters');
-
-    termLetters = selectedTerm['term'].split("");
-    console.log('termLetters: ', termLetters);
-
-    termLetterSet = new Set(termLetters);
-    console.log('termLetterSet:', termLetterSet);
-
-    alphabet = ['A', 'B', 'C', 'D',
-        'E', 'F', 'G',
-        'H', 'I', 'J', 'K', 'L', 'M', 'N',
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-        'V', 'W', 'X', 'Y', 'Z'
-    ];
-
-    alphabetSet = new Set(alphabet);
-    console.log('alphabetSet: ', alphabetSet);
-
-    availableLetterSet = new Set(alphabet);
-    console.log('availableLetterSet: ', availableLetterSet);
-
-    usedLetterSet = new Set(); // begins as empty set
-    console.log('initial usedLetterSet: ', usedLetterSet);
-
-    numberBadGuesses = 0; // initialize count prior to for-loop
-    continueGame = true; // Boolean switch use in while loop
-    outcomeMessage = ''; // initialize message showing game outcome
-    while ((numberBadGuesses < numberGuessesAllowed) && continueGame) {
+    var americanGameSet = americanIconSet;  // initialize
+    var nationalGameSet = nationalIconSet;  // initialize
+    indexOfGame = 0;
+    while (indexOfGame < numberGames) {
         console.log('-----------------------------');
-        availableLetterArray = Array.from(availableLetterSet);
-        lettersAvailable.innerHTML =
-             'Letters available: ' + availableLetterArray.join(' ');
 
-        usedLetterArray = Array.from(usedLetterSet);
-        lettersUsed.innerHTML =
-             'Letters used: ' + usedLetterArray.join(' ');
-    // check to see that the response is valid... 
-    // must be a single letter and cannot be a letter 
-    // that was chosen previously    
-        responseValid = false;
-        while (!responseValid) {
-            promptGuessLetter = 
-                prompt('guess a letter').toUpperCase();
-            if((alphabetSet.has(promptGuessLetter)) &&
-                (!usedLetterSet.has(promptGuessLetter)))
-                responseValid = true;
-        };
-        
-        console.log('promptGuessLetter: ', promptGuessLetter);
-        // this must be a single letter... no other characters permitted
-        // we convert the typed letter to uppercase
-        // with each guess we add to the usedLetterSet
-        // and delete from the availableLetterSet 
-        thisGuessLetter = promptGuessLetter.toUpperCase();
-        console.log('thisGuessLetter: ', thisGuessLetter);
-
-        usedLetterSet.add(thisGuessLetter);
-        console.log('current usedLetterSet: ', usedLetterSet);
-
-        availableLetterSet.delete(thisGuessLetter);
-        console.log('current availableLetterSet:', availableLetterSet);
-
-        // if the guessLetter is in the termLetterSet then
-        //   we take appropriate action to display updated target
-
-        // if the guessLetter is not in the termLetterSet then
-        //   we increment the numberBadGuesses 
-
-        if (termLetterSet.has(thisGuessLetter)) {
-            // update the target display by adding correct guess letter
-            for (i = 0; i < numberTargetLetters; i++) {
-                if (thisGuessLetter === termLetters[i]) {
-                    target[i] = thisGuessLetter
-                }
-            }
-
-            
-
-            // if there are no longer any blank characters in the target
-            // then the game has been won and we pop out of the while-loop
-            targetSet = new Set(target);
-            if (!targetSet.has('_')) {
-                
-                continueGame = false;
-                availableLetterArray = Array.from(availableLetterSet);
-                lettersAvailable.innerHTML =
-                    'Letters available: ' + availableLetterArray.join(' ');
-
-                usedLetterArray = Array.from(usedLetterSet);
-                lettersUsed.innerHTML =
-                   'Letters used: ' + usedLetterArray.join(' ');
-
-                console.log('current target display:', target.join(' '));
-                
-            } 
-        } else {
-            numberBadGuesses++;
-            console.log('bad guess');
-            console.log('numberBadGuesses: ', numberBadGuesses)
-            availableLetterArray = Array.from(availableLetterSet);
-            lettersAvailable.innerHTML =
-                    'Letters available: ' + availableLetterArray.join(' ');
-
-            usedLetterArray = Array.from(usedLetterSet);
-            lettersUsed.innerHTML =
-                   'Letters used: ' + usedLetterArray.join(' ');
-
-            console.log('current target display:', target.join(' '));
-
-        } 
+        // choose teams at random without replacement
 
     }; // end of major while-loop
 
-    // if finish your alloted numberGuessesAllowed without
-    // guessing all the letters... then you lose the game
-    // ... but the correct answer is shown with definition
-
     // report result of game to DOM
-    if (numberBadGuesses === numberGuessesAllowed) {
-        console.log("report game outcome: Better Luck Next Time");
-        statusText.innerHTML = 'Better Luck Next Time';
-    }
-    else {
-        console.log("report game outcome: You Win");
-        statusText.innerHTML = 'You Win';
-    }
-
-    correctTerm.innerHTML = ('The correct word is ' + selectedTerm['term'] + '.');
-
-    definitionText.innerHTML = selectedTerm['definition'];
+ 
+    gameResultText.innerHTML = ('You chose ' + ' American League Teams and ' + 
+        ' National League Teams');
   
 } // startButton.onclick function end
 
